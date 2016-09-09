1afd0e5c02a8014b50051a8ed9b0855953de4618
pat-all <topalov.petr@gmail.com>
2016-09-09 18:18:18 +0300

    Star code.

---
+++ controllers/CartController.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * CartController.php
+ *
+ * controller for cart
+ */
+
+//including models
+include_once "../models/CategoriesModel.php";
+include_once "../models/ProductsModel.php";
+
+function addToCartAction($itemId){
+    $itemId = isset($_GET["id"]) ? intval($_GET["id"]) : null;
+    if (! $itemId) return false;
+
+    $resData = array();
+
+    if(isset($_SESSION["cart"]) && array_search($itemId, $_SESSION["cart"]) === false){
+        $_SESSION["cart"][] = $itemId;
+        $resData["cntItems"] = count($_SESSION["cart"]);
+        $resData["success"] = 1;
+    } else {
+        $resData["success"] = 0;
+    }
+
+    echo json_encode($resData);
+}
---
+++ www/js/main.js
@@ -0,0 +1,18 @@
+function addToCart(itemId) {
+    console.log("js - addToCart()");
+    $.ajax({
+        type: "POST",
+        async: false,
+        url: "/Cart/addToCart/" + itemId + "/",
+        dataType: "json",
+        success: function (data) {
+            if(data["success"]){
+                $("#cartCntItems").html(data["cntItems"]);
+
+                $("#addCart_" + itemId).hide();
+                $("#removeCart_" + itemId).show();
+                console.log("ajax success");
+            }
+        }
+    });
+}
